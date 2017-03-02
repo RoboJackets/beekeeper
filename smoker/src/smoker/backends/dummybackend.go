311a64eedbbd90a581661c543ae532cd93db898c
Jay Kamat <jaygkamat@gmail.com>
2017-03-02 16:23:14 -0500

    Add initial work on frontend

--- smoker/src/smoker/backends/dummybackend.go
+++ smoker/src/smoker/backends/dummybackend.go
@@ -1,49 +1,126 @@
 package backends
 
-// import "fmt"
+import (
+	"errors"
+	"strconv"
+)
 
 // Represents a bin
 type Bin struct {
-	name string
-	// Actually a set
-	parts *map[Component]bool
+	name     string
+	capacity uint
+
+	parts map[*Component]bool
+}
+
+func (b *Bin) GetName() string {
+	return b.name
+}
+func (b *Bin) GetCapacity() uint {
+	return b.capacity
+}
+
+func (b *Bin) GetParts() []Component {
+	comp := make([]Component, 0)
+	for v := range b.parts {
+		comp = append(comp, *v)
+	}
+
+	return comp
 }
 
 // Represents a component
 type Component struct {
-	id    uint
-	owner *Bin
+	id, count uint
+	owner     *Bin
+
+	name, manufacturer string
+}
+
+func (c *Component) GetName() string {
+	return c.name
+}
+func (c *Component) GetManufacturer() string {
+	return c.manufacturer
+}
+func (c *Component) GetId() uint {
+	return c.id
 }
 
 type DummyBackend struct {
 	// Map of component ID to component
 	// Use this for most lookups
-	idLookup *map[uint]Component
+	idLookup map[uint]*Component
 	// Set of all components (for searches)
-	components *map[Component]bool
+	components map[*Component]bool
 	// List of all the bins
 	bins []Bin
 }
 
 // Makes a very simple backend.
-func NewDummyBackend() *DummyBackend {
+// If specifying a number <= 0, 1 is defaulted to
+func NewDummyBackend(numBins uint) *DummyBackend {
+	if numBins <= 0 {
+		numBins = 1
+	}
 
-	idLookup := make(map[uint]Component)
-	components := make(map[Component]bool)
-	newDummy := DummyBackend{idLookup: &idLookup, components: &components, bins: make([]Bin, 10)}
+	idLookup := make(map[uint]*Component)
+	components := make(map[*Component]bool)
+	newDummy := DummyBackend{
+		idLookup:   idLookup,
+		components: components,
+		bins:       make([]Bin, numBins)}
 
 	// Let's make 10 bins
 	for i := range newDummy.bins {
-		mp := make(map[Component]bool)
+		mp := make(map[*Component]bool)
 		newDummy.bins[i] = Bin{
-			name:  "Name",
-			parts: &mp}
+			name:  "A" + strconv.Itoa(i),
+			parts: mp,
+			// TODO stop hard coding this
+			capacity: 3}
 	}
 
 	return &newDummy
 }
 
-// TODO remove me
-func Add(x, y int) int {
-	return (x + y)
+func NewComponent(id, count uint, name, manufacturer string) *Component {
+	return &Component{
+		id:           id,
+		count:        count,
+		name:         name,
+		manufacturer: manufacturer}
+}
+
+func (b *DummyBackend) AddComponent(comp *Component) error {
+	var selectedBin *Bin
+	for _, v := range b.bins {
+		if v.capacity > uint(len(v.parts)) {
+			selectedBin = &v
+			break
+		}
+	}
+	if selectedBin == nil {
+		return errors.New("No more space in bins!")
+	}
+
+	// Actually add component to bin
+	selectedBin.parts[comp] = true
+	comp.owner = selectedBin
+
+	// Add lookup pointers for us
+	b.idLookup[comp.id] = comp
+	b.components[comp] = true
+	return nil
+}
+
+func (b *DummyBackend) LookupId(id uint) (*Component, *Bin, error) {
+	if component, present := b.idLookup[id]; !present {
+		return nil, nil, errors.New("No component found with that ID.")
+	} else {
+		if component.owner == nil {
+			return nil, nil, errors.New("[INTERNAL] The component found has no bin associated with it.")
+		}
+		return component, component.owner, nil
+	}
 }
--- smoker/src/smoker/smoker/smoker.go
+++ smoker/src/smoker/smoker/smoker.go
@@ -5,15 +5,51 @@ import (
 	"fmt"
 	"os"
 	"smoker/backends"
+	_ "color"
 )
+
+const INTRO_TEXT =
+	`Welcome to Smoker - The superior beehive client
+   _________ ___  ____  / /_____  _____
+  / ___/ __ `+"`" + `__ \/ __ \/ //_/ _ \/ ___/
+ (__  ) / / / / / /_/ / ,< /  __/ /
+/____/_/ /_/ /_/\____/_/|_|\___/_/
+
+for help, type 'help'`
 
 // Main function for smoker
 func main() {
-	fmt.Println("hello world")
-	x := backends.Add(1, 2)
-	fmt.Println(x)
+	intro()
+
+	dummy := backends.NewDummyBackend(10)
+	fmt.Println(dummy)
+
+	component := backends.NewComponent(1, 1, "30 Ω Resistor", "DigiKey")
+	dummy.AddComponent(component)
+	if c, b, e := dummy.LookupId(1); e != nil {
+		fmt.Println("that part was not found")
+	} else {
+		fmt.Println(c)
+		fmt.Println(b)
+		// parts := b.GetParts()
+		// fmt.Println(parts)
+		// fmt.Println(b)
+		// fmt.Println(c)
+	}
+
+}
+
+func intro() {
+	fmt.Println(INTRO_TEXT)
+}
+
+// Reads a line of input
+func read(s string) string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	fmt.Print(s)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	return text
 }
