c6d0835ef4d71b865f8c17879bc33791fa22de17
Will Stuckey <guyfleeman@gmail.com>
2017-03-05 19:17:44 -0500

    initial resource structure

---
+++ warre/index.js
@@ -0,0 +1,9 @@
+/**
+ * @author willstuckey
+ * @date 3/5/17.
+ */
+
+function page_init() {
+	// inject navbar
+	$("#nav_inject").load("navbar.html");
+}
---
+++ warre/js/global_on_page_load.js
@@ -0,0 +1,11 @@
+/**
+ * @author willstuckey
+ * @date 3/4/17.
+ */
+
+function on_page_ready() {
+	//TODO: check redirect
+
+	// calls page init function from corresponding page js file
+	page_init();
+}
